e65d86b20aa45a266c6a4b430ce9eb02d3d9942a
grimlink <sean.grimlink@gmail.com>
2026-05-15 03:53:41 +0200

    ADD: beter meta

---
+++ src/components/Schema/index.ts
@@ -0,0 +1,5 @@
+import Schema from "@fylgja/Schema.astro";
+import Me from "./Me.astro";
+import Site from "./Site.astro";
+
+export { Schema as default, Schema, Me as SchemaMe, Site as SchemaSite };
--- src/consts.ts
+++ src/consts.ts
@@ -1,34 +1,43 @@
-export const siteTitle = "GrimLink.com";
+export const siteTitle = "GrimLink";
 export const siteDescription =
-  "Hi, I am Sean van Zuidam also known on the web as GrimLink. I am a Frontend Developer from Zwollywood.";
-export const author = "Sean van Zuidam";
+	"Hi, I am Sean van Zuidam also known on the web as GrimLink. I am a Frontend Developer from Zwollywood.";
 export const themeColor = "#fff";
+
+export const author = "Sean van Zuidam";
+export const authorAbout =
+	"Frontend Developer, CSS maniac, Gamer and also the creator of Fylgja CSS";
+export const authorTitle = "CSS Wizard";
 
 export const today = new Date();
 
 export const socials = {
-  github: "https://github.com/GrimLink/",
-  bluesky: "https://bsky.app/profile/grimlink.bsky.social/",
-  mastodon: "https://mastodon.social/@GrimLink/",
-  linkedin: "https://www.linkedin.com/in/seanvanzuidam/",
-  instagram: "https://www.instagram.com/seangrimlink/",
-  youtube: "https://www.youtube.com/@SeanGrimLink/",
+	github: "https://github.com/GrimLink/",
+	bluesky: "https://bsky.app/profile/grimlink.bsky.social/",
+	mastodon: "https://mastodon.social/@GrimLink/",
+	linkedin: "https://www.linkedin.com/in/seanvanzuidam/",
+	instagram: "https://www.instagram.com/seangrimlink/",
+	youtube: "https://www.youtube.com/@SeanGrimLink/",
 };
 
 export const myLists = {
-  Backloggd: "https://backloggd.com/u/Grimlink/",
-  MyAnimelist: "https://myanimelist.net/profile/GrimLink/",
+	Backloggd: "https://backloggd.com/u/Grimlink/",
+	MyAnimelist: "https://myanimelist.net/profile/GrimLink/",
 };
 
 export const projects = [
-  {
-    title: "Fylgja CSS",
-    description: "",
-    url: "https://fylgja.dev/",
-  },
-  {
-    title: "Mage Script",
-    description: "",
-    url: "https://github.com/grimlink/mage/",
-  },
+	{
+		title: "Fylgja CSS",
+		description: "",
+		url: "https://fylgja.dev/",
+	},
+	{
+		title: "Magento2 Storeinfo",
+		description: "",
+		url: "https://github.com/Siteation/magento2-storeinfo",
+	},
+	{
+		title: "Mage Script",
+		description: "",
+		url: "https://github.com/grimlink/mage/",
+	},
 ];
