f31548b20479238768d78ce3956ec0cdcc92e789
grimlink <sean.grimlink@gmail.com>
2026-05-15 18:25:20 +0200

    DEL: test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
 	site: "https://grimlink.com",
-	base: "/grimlink.com",
 	integrations: [sitemap()],
 	devToolbar: {
 		enabled: false,
