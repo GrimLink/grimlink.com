9fc4233c5efa58bca9fdd7886aaf32851aa1ce4b
grimlink <sean.grimlink@gmail.com>
2026-05-15 18:05:22 +0200

    ADD: base for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://grimlink.com",
-  integrations: [sitemap()],
-  devToolbar: {
-    enabled: false,
-  },
+	site: "https://grimlink.com",
+	base: "/grimlink.com",
+	integrations: [sitemap()],
+	devToolbar: {
+		enabled: false,
+	},
 });
