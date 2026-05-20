d16aa89be7cf3733ba73b6245ab3fb9b83439638
grimlink <sean.grimlink@gmail.com>
2026-05-20 23:57:48 +0200

    ADD Enable remote images and create speaking page
    Allow HTTPS remote images in Astro config. Add Youtube embed component,
    speaking events data, and speaking page showcasing events and video
    appearances.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
 	site: "https://grimlink.com",
 	integrations: [sitemap()],
+	image: {
+		remotePatterns: [{ protocol: "https" }],
+	},
 	devToolbar: {
 		enabled: false,
 	},
