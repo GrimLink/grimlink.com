263aded0ed7b57428a99260027cee7a506ad0c0c
grimlink <sean.grimlink@gmail.com>
2026-06-03 12:26:31 +0200

    IMP: page schema

--- src/components/Schema/index.ts
+++ src/components/Schema/index.ts
@@ -1,5 +1,14 @@
 import Schema from "@fylgja/Schema.astro";
 import Me from "./Me.astro";
 import Site from "./Site.astro";
+import BlogPosting from "./BlogPosting.astro";
+import Breadcrumbs from "./Breadcrumbs.astro";
 
-export { Schema as default, Schema, Me as SchemaMe, Site as SchemaSite };
+export {
+	Schema as default,
+	Schema,
+	Me as SchemaMe,
+	Site as SchemaSite,
+	BlogPosting as SchemaBlogPosting,
+	Breadcrumbs as SchemaBreadcrumbs,
+};
