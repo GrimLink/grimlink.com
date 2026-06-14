171dee795fd0a01854f15dbd079fb4c63b720f72
grimlink <sean.grimlink@gmail.com>
2026-06-14 16:08:08 +0200

    IMP: post types and options

--- src/content.config.ts
+++ src/content.config.ts
@@ -6,11 +6,12 @@ const blog = defineCollection({
 	loader: glob({ base: "./src/content/blog", pattern: "**/*.md" }),
 	schema: ({ image }) =>
 		z.object({
-			title: z.string(),
-			description: z.string(),
+			title: z.string().min(10).max(64),
+			description: z.string().max(160),
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			image: z.optional(image()),
+			thumb: z.optional(image().describe("Overrides the image to set a specific blog thumb")),
 			tags: z.array(z.string()).optional(),
 			crossPosts: z.record(z.string(), z.string()).optional(),
 		}),
