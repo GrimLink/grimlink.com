2093b5e42975d961ee964d34af9cb1dce7037814
grimlink <sean.grimlink@gmail.com>
2026-06-22 09:34:05 +0200

    ADD: more pages

--- src/menus.ts
+++ src/menus.ts
@@ -1,14 +1,18 @@
 import { socials } from "./consts";
 
 export const mainMenu = [
-	{ url: "/blog/", label: "My Feed" },
-	{ url: "/speaking/", label: "Speaking" },
-	{ url: socials.github, label: "GitHub" },
-	{ url: "/about/", label: "About Me" },
+	{ url: "/blog/", label: "My Feed", icon: "Newspaper" },
+	{ url: "/speaking/", label: "Speaking", icon: "Calendar" },
+	{ url: "/projects/", label: "Projects", icon: "FolderOpen" },
+	{ url: "/games/", label: "Games", icon: "Gamepad2" },
+	{ url: "/about/", label: "About Me", icon: "User" },
+	{ url: socials.github, label: "GitHub", icon: "Github" },
+	{ url: "/404/", label: "404", icon: "Ghost" },
 ];
 
 export const footerMenu = [
 	{ url: "/blog/", label: "My Feed" },
 	{ url: "/speaking/", label: "Speaking" },
+	{ url: "/projects/", label: "Projects" },
 	{ url: "/about/", label: "About Me" },
 ];
